41882086d874734eb3b5fcf41c620d93e41f24a8
Zechariah <zechariahtan144@gmail.com>
2021-12-17 17:29:54 +0800

    Moved download route to express due to response size limit

---
+++ src/app.ts
@@ -0,0 +1,66 @@
+import express from "express"
+import ffmpeg from "fluent-ffmpeg"
+import next from "next"
+import ytdl from "ytdl-core"
+import { OBJECT, STRING, validate } from "validate-any"
+
+const PORT = 1902
+const dev = process.env.NODE_ENV !== "production"
+
+const app = next({ dev })
+const handle = app.getRequestHandler()
+ffmpeg.setFfmpegPath(require("@ffmpeg-installer/ffmpeg").path)
+
+app.prepare().then(() => {
+	const app = express()
+
+	app.get("/api/download", (req, res) => {
+		const { success, errors, data } = validate(
+			req.query,
+			OBJECT({
+				url: STRING(),
+				format: STRING("videoandaudio", "audioonly"),
+				name: STRING(),
+				bitrate: STRING()
+			}),
+			"URL"
+		)
+
+		if (!success) {
+			res.status(400).send(
+				"The Following errors exist in the URL:" + errors.map(e => "\n - " + e).join("")
+			)
+			return
+		}
+
+		const { url, format, name, bitrate } = data!
+		const stream = ytdl(url, {
+			filter: format,
+			quality: "highest"
+		})
+
+		res.setHeader(
+			"Content-Disposition",
+			`attachment; filename="${name}.${format === "audioonly" ? "mp3" : "mp4"}"`
+		)
+
+		if (format === "videoandaudio") {
+			stream.pipe(res).on("error", err => {
+				console.error(err.message)
+			})
+		} else {
+			ffmpeg(stream)
+				.audioBitrate(bitrate)
+				.withAudioCodec("libmp3lame")
+				.toFormat("mp3")
+				.pipe(res)
+				.on("error", err => {
+					console.error(err.message)
+				})
+		}
+	})
+
+	app.all("*", (req, res) => handle(req, res))
+
+	app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+})
