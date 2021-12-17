d06723e7461adbbf8cd49f89c717c37d8722657e
Zechariah <zechariahtan144@gmail.com>
2021-12-17 17:38:36 +0800

    Ensure filename isn't empty

--- src/pages/api/validate/filename.ts
+++ src/pages/api/validate/filename.ts
@@ -9,6 +9,12 @@ const handler = (req: NextApiRequest, res: NextApiResponse<Data>) => {
 
 	const filename = req.body.filename as string
 
+	if (filename === "") {
+		return res.status(400).send({
+			message: "Filename cannot be empty"
+		})
+	}
+
 	try {
 		res.setHeader("Content-Disposition", `attachment; filename="${filename}.mp3"`)
 		res.status(200).end()
