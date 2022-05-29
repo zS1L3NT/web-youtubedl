07f477d67677b1a8255960c199c6da74c32512fe
Zechariah <zechariahtan144@gmail.com>
2022-05-30 00:51:12 +0800

    ✨ fix peer dependencies

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,5 @@
 import "./index.css"
 
-import axios from "axios"
 import { StrictMode } from "react"
 import ReactDOM from "react-dom"
 
@@ -9,8 +8,6 @@ import { ThemeProvider } from "@mui/material"
 import App from "./App"
 import ContextProviders from "./contexts/ContextProviders"
 import theme from "./theme"
-
-axios.defaults.baseURL = "http://localhost:1902"
 
 ReactDOM.hydrate(
 	<StrictMode>
