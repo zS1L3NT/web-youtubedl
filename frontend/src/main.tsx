df3b9545d61032a11079a55edc9438348247ccf6
Zechariah <zechariahtan144@gmail.com>
2022-08-02 02:06:11 +0800

    🐛 fix wrong axios base url error

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -10,7 +10,7 @@ import App from "./App"
 import ContextProviders from "./contexts/ContextProviders"
 import theme from "./theme"
 
-axios.defaults.baseURL = "http://localhost:1902"
+axios.defaults.baseURL = "http://ytdl.zectan.com"
 
 ReactDOM.hydrate(
 	<StrictMode>
