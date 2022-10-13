f2274b60b9902dfd196e4301cbc64479d7e39ff6
Zechariah <zechariahtan144@gmail.com>
2022-10-14 04:50:46 +0800

    ✨ deploy to fly.io

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -10,7 +10,7 @@ import { iRoute } from "./setup"
 dotenv.config()
 ffmpeg.setFfmpegPath(require("@ffmpeg-installer/ffmpeg").path)
 
-const PORT = process.env.PORT || 1902
+const PORT = process.env.PORT || 8080
 const app = express()
 
 if (process.env.NODE_ENV !== "production") {
